6095cfa66011fa7b19b464996b655935a6394bb4
cuyfer <161590985+cuyfer@users.noreply.github.com>
2024-10-13 09:57:56 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'Chat App';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './pplg3Logo.jpeg',
+  './pplg3Logo.jpeg',
+];
+
+// Instal service worker dan cache resource
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Aktifkan service worker dan hapus cache lama
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Tangani fetch request dan berikan cache saat offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
